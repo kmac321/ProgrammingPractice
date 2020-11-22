5d54f6082d05745a22fff007764a2148061bc2f2
kmac321 <64026121+kmac321@users.noreply.github.com>
2020-11-22 16:03:57 +0000

    Added Song class with properties and enum

---
+++ Programming Practice/Song.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_Practice
+{
+    public class Song
+    {
+        //properties
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public double Duration { get; set; }
+
+        public Genre MusicGenre { get; set; }
+
+        //constructors
+
+        //methods
+    }
+
+    public enum Genre { Rock, Pop, Dance, Other}
+}
