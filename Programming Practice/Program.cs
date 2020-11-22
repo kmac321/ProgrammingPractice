e7229fd0053c9d6e5209df087d78272f02f894a7
kmac321 <64026121+kmac321@users.noreply.github.com>
2020-11-22 16:24:58 +0000

    Created song objects and list to hold them

--- Programming Practice/Program.cs
+++ Programming Practice/Program.cs
@@ -10,6 +10,26 @@ namespace Programming_Practice
     {
         static void Main(string[] args)
         {
+            Song s1 = new Song("BEAUTIFUL PEOPLE", "ED SHEERAN", 3.15, Genre.Pop);
+            Song s2 = new Song("DANCE MONKEY", "TONES & 1", 4.20, Genre.Dance);
+            Song s3 = new Song("CIRCLES", "POST MALONE", 3.25, Genre.Pop);
+            Song s4 = new Song("RIDE IT", "REGARD", 3.37, Genre.Dance);
+            Song s5 = new Song("SOUTH OF THE BORDER", "ED SHEERAN", 4.26, Genre.Pop);
+
+            List<Song> playlist = new List<Song>();
+            
+            playlist.Add(s1);
+            playlist.Add(s2);
+            playlist.Add(s3);
+            playlist.Add(s4);
+            playlist.Add(s5);
+
+            foreach (Song song in playlist)
+            {
+                Console.WriteLine(song);
+            }
+
+
         }
     }
 }
--- Programming Practice/Song.cs
+++ Programming Practice/Song.cs
@@ -12,12 +12,26 @@ namespace Programming_Practice
         public string Title { get; set; }
         public string Artist { get; set; }
         public double Duration { get; set; }
-
         public Genre MusicGenre { get; set; }
 
         //constructors
+        public Song(string title, string artist, double duration, Genre musicGenre)
+        {
+            Title = title;
+            Artist = artist;
+            Duration = duration;
+            MusicGenre = musicGenre;
+        }
+
+        public Song(string title, string artist) : this(title, artist, 0, Genre.Other) { }
+
+        public Song() : this("Unknown", "Unknown") { }
 
         //methods
+        public override string ToString()
+        {
+            return string.Format($"{Title} {Artist} {Duration} {MusicGenre}");
+        }
     }
 
     public enum Genre { Rock, Pop, Dance, Other}
